07536e3d768113b98ddf87c3622055ec0f9a87ae
Christopher Arias <christopher.arias.jr@gmail.com>
2021-05-27 12:08:59 -0700

    Cleaned up and added projects.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,51 @@
 <template>
   <div id="app">
     <Navbar />
-    <router-view/>
+    <div class="home">
+      <div class="container">
+        <div class="row">
+          <div class="col text-center header">
+            <h1>{ Hello, World }</h1>
+          </div>
+        </div>
+        <main>
+          <div class="row">
+            <div class="col">
+              <div class="about">
+                <div class="row">
+                  <div class="col">
+                    <h4>Christopher Arias</h4>
+                    <a href="mailto:christopher.arias.jr@gmail.com">Christopher.arias.jr@gmail.com</a>
+                  </div>
+                  <div class="col text-right">
+                    <h5 style="position: absolute; bottom: -11px; right: 18px;">Web-Developer</h5>
+                  </div>
+                </div>
+                <hr />
+                <span>
+                  Born and raised in Arizona. Loves to code and develop software. Quick and effecient. Works well with people. Coffee might be accepted as a form of payment. 
+                </span>
+              </div>
+              <div class="social">
+                <h4>Social Sites</h4>
+                <hr>
+                <ul>
+                  <li><a href="https://github.com/christopherariasjr">Github</a></li>
+                  <li><a href="https://www.linkedin.com/in/christopher-arias-66b959176/">Linkedin</a></li>
+                </ul>
+              </div>
+              <div class="projects">
+                <h4>Projects</h4>
+                <hr>
+                <ul class v-for="item in projects" :key="item.name">
+                  <li>{{item.name}} - <a :href="item.url">{{item.url}}</a></li>
+                </ul>
+              </div> 
+            </div>
+          </div>
+        </main>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,10 +53,23 @@
 import Navbar from './components/Navbar'
 
 export default {
-
   components: {
     Navbar
   },  
+  data: () => {
+    return {
+      projects: [
+        {
+          url: "http://bitwatcher.ariaslabs.com",
+          name: "Bitwatcher"
+        },
+        {
+          url: "https://weathertrack.ariaslabs.com",
+          name: "Weathertrack"
+        }
+      ]
+    }
+  }
 }
 </script>
 
@@ -29,9 +86,55 @@ export default {
 }
 
 
-h1, h2, h3, h4 h5, h6 {
-  font-family: 'Share Tech Mono', monospace;
-}
+  h1, h2, h3, h4 h5, h6 {
+    font-family: 'Share Tech Mono', monospace;
+  }
 
+  .home {
+    margin-top: 14em;
+  }
 
+  main {
+    max-width: 600px;
+    margin-left: auto;
+    margin-right: auto;
+    padding-left: 10px;
+    margin-bottom: 5em;
+  }
+
+  .header {
+    padding-left: 20px !important;
+  }
+
+  .about {
+    position: relative;
+    margin-top: 18em;
+    
+  }
+
+  .social {
+    margin-top: 5em;
+  }
+
+  .projects {
+    margin-top: 5em;
+  }
+
+  hr {
+    color: white !important ;
+    background:  white;
+  }
+
+  a {
+     color: white !important;
+     text-decoration: underline !important;
+  }
+
+  a:hover {
+    color: rgb(255, 216, 87) !important;
+  }
+
+  ul {
+    list-style-type: none;
+  }
 </style>
